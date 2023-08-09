991a0d3cc9a5cc1cd94fd4dbe779e8995b9a8a88
tim.sie <tim.sie@imsoft.com.tw>
2023-08-09 15:18:32 +0800

    增加 ChatSocketService 功能彈性

--- src/app/@service/chat-socket.service.ts
+++ src/app/@service/chat-socket.service.ts
@@ -1,13 +1,7 @@
-import { Injectable } from '@angular/core';
-import { AppComponent } from '../app.component';
 //使用 SockJs 需要引入以下兩個套件
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 
-
-@Injectable({
-  providedIn: 'root'
-})
 export class ChatSocketService {
   
   //宣告傳送資料給後端server(SpringBoot)的URL位置。
@@ -20,11 +14,11 @@ export class ChatSocketService {
   stompClient: any;
 
   //宣告一個變數，存放前端的Component。
-  appComponent: AppComponent;
+  socketController: SocketController;
 
   //建構子 取得前端的Component
-  constructor(appComponent: AppComponent){
-      this.appComponent = appComponent;
+  constructor(socketController: SocketController){
+      this.socketController = socketController;
   }
 
   /* 
@@ -51,9 +45,9 @@ export class ChatSocketService {
               第二個是從後端 Server 接收到資料後要執行的方法，型態為 function 其參數(sdkEvent)為後端傳過來的資料。
         */
         _this.stompClient.subscribe(_this.topic, function (sdkEvent: any) {
-            _this.onMessageReceived(sdkEvent);
+            _this.socketController.onMessageReceived(sdkEvent);
         });
-    }, this.errorCallBack);
+    }, this.socketController.errorCallBack);
   };
 
   //關閉Socket連線的方法
@@ -62,15 +56,6 @@ export class ChatSocketService {
         this.stompClient.disconnect();
     }
     console.log("連線關閉~");
-  }
-
-  // 發生異常時執行的方法
-  errorCallBack(error:any) {
-      console.log("連線異常 -> " + error);
-      //設定一段時間後重新連線。
-      setTimeout(() => {
-          this._connect();
-      }, 5000);
   }
 
   /**
@@ -87,12 +72,12 @@ export class ChatSocketService {
        */
       this.stompClient.send("/app/chatIn", {}, JSON.stringify({"message" : message}));
   }
-
+}
+//新增介面類，增加程式彈性，降低耦合。
+export interface SocketController{
   //收到Service傳送過來的訊息後執行的方法。
-  onMessageReceived(message:any) {
-      console.log("Message Recieved from Server :: " + message);
-      //取得server傳過來的訊息，裡面的body並將body內的資料轉成Json格式。
-      this.appComponent.handleMessage(JSON.parse(message.body));
-  }
+  onMessageReceived(message:any):void;
 
+  // 發生異常時執行的方法
+  errorCallBack(error:any):void;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
-import { ChatSocketService } from './@service/chat-socket.service';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { ChatSocketService, SocketController } from './@service/chat-socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewChecked {
+export class AppComponent implements OnInit, AfterViewChecked, SocketController{
   @ViewChild('scrollMe')
   private scrollMe!: ElementRef;
 
@@ -39,8 +39,21 @@ export class AppComponent implements OnInit, AfterViewChecked {
     input.value = "";
   }
 
-  handleMessage(message:any){
-    console.log(message.content);
-    this.greeting.push(message.content);
+  //實作SocketController，收到Service傳送過來的訊息後執行的方法。
+  onMessageReceived(message: any){
+    //取得service傳過來的資料，request並將body內的資訊轉換為Json格式。
+    const data = JSON.parse(message.body);
+    
+    console.log(data.content);
+    this.greeting.push(data.content);
+  }
+  
+  //實作SocketController，發生異常時執行的方法。
+  errorCallBack(error: any){
+    console.log("連線異常 -> " + error);
+    //設定一段時間後重新連線。
+    setTimeout(() => {
+      this.connect();
+    }, 5000);
   }
 }
